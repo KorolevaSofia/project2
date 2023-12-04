c4e0c920dd4390977accec9be0a2f5804efc79d3
Sonya K <magvanss@yandex.ru>
2023-12-04 13:31:29 +0300

    initial commit

---
+++ src/Main.java
@@ -0,0 +1,9 @@
+public class Main {
+    public static void main(String[] args) {
+        int a = 13696;
+        int b = 20;
+        int c = a / b;
+        System.out.print("Количество начисленных миль =");
+        System.out.print(c);
+    }
+}
